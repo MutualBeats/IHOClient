f9fc96c8b9e660c4bc7186727ecf6f3be6a600cb
hhxianqu <735913885@qq.com>
2016-11-06 17:02:03 +0800

    .

--- src/main/java/bussinesslogicservice/promotionblservice/MemberPromotionBLservice.java
+++ src/main/java/bussinesslogicservice/promotionblservice/MemberPromotionBLservice.java
@@ -5,10 +5,17 @@ import java.util.ArrayList;
 import vo.MemberVO;
 
 public interface MemberPromotionBLservice {
+	/*
+	 * 根据输入的信用值划分等级
+	 * */
 	public ArrayList <Integer> levelMake (MemberVO vo);
-	
-	public void getLevel(MemberVO vo);
-	
-	public void getDiscount(MemberVO vo);
+	/*
+	 * 返回会员等级信息
+	 * */
+	public MemberVO getLevel (String userID);
+	/*
+	 * 获取会员折扣信息
+	 * */
+	public MemberVO getDiscount (String userID);
 	
 }
---
+++ src/main/java/bussinesslogicservice/promotionblservice/MemberPromotionbl_Stub.java
@@ -0,0 +1,27 @@
+package bussinesslogicservice.promotionblservice;
+
+import java.util.ArrayList;
+
+import vo.MemberVO;
+
+public class MemberPromotionbl_Stub implements MemberPromotionBLservice{
+
+	@Override
+	public ArrayList<Integer> levelMake(MemberVO vo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public MemberVO getLevel (String userID) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public MemberVO getDiscount (String userID) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
