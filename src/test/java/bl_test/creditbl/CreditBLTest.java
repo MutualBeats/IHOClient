bf4eed5422ba534461967f89335f687d294b0b74
YYM <979579683@qq.com>
2016-11-19 19:34:52 +0800

    测试信息隐藏

--- src/test/java/bl_test/creditbl/CreditBLTest.java
+++ src/test/java/bl_test/creditbl/CreditBLTest.java
@@ -49,22 +49,22 @@ public class CreditBLTest {
 	@Test
 	public void creditIncreaseTest() {
 		assertEquals(credit.increaseCredit("12345678", -10010), ResultMessage.CreditIncreaseFailed);
-		String errorInfo = err_content.toString();
-		assertEquals(errorInfo, "Error ! Value change of credit to increase must be positive !\r\n");
+//		String errorInfo = err_content.toString();
+//		assertEquals(errorInfo, "Error ! Value change of credit to increase must be positive !\r\n");
 		assertEquals(credit.increaseCredit("12345678", 10010), ResultMessage.CreditIncreaseSuccess);
-		String okInfo = out_content.toString();
-		assertEquals(okInfo, "Find Succeed!\r\nInsert Succeed!\r\n");
+//		String okInfo = out_content.toString();
+//		assertEquals(okInfo, "Find Succeed!\r\nInsert Succeed!\r\n");
 //		credit.in
 	}
 	
 	@Test
 	public void creditDecreaseTest() {
 		assertEquals(credit.decreaseCredit("12345678", 10010), ResultMessage.CreditDecreaseFailed);
-		String errorInfo = err_content.toString();
-		assertEquals(errorInfo, "Error ! Value change of credit to decrease must be negative !\r\n");
+//		String errorInfo = err_content.toString();
+//		assertEquals(errorInfo, "Error ! Value change of credit to decrease must be negative !\r\n");
 		assertEquals(credit.decreaseCredit("12345678", -10010), ResultMessage.CreditDecreaseSuccess);
-		String okInfo = out_content.toString();
-		assertEquals(okInfo, "Find Succeed!\r\nInsert Succeed!\r\n");
+//		String okInfo = out_content.toString();
+//		assertEquals(okInfo, "Find Succeed!\r\nInsert Succeed!\r\n");
 	}
 	
 	@Test
