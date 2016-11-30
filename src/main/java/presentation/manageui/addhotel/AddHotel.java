6e02c0f14a7febc355e63c336335b34b564a8bed
YYM <979579683@qq.com>
2016-11-30 20:48:44 +0800

    配置文件及xml包更新

--- src/main/java/presentation/manageui/addhotel/AddHotel.java
+++ src/main/java/presentation/manageui/addhotel/AddHotel.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package presentation.manageui.addHotel;
+package presentation.manageui.addhotel;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
