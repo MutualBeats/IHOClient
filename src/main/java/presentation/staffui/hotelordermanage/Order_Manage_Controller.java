68a48ddfb0cf945631991650a6ff021ce397c91a
hhxianqu <apple@appledeMacBook-Pro-6.local>
2016-12-05 12:33:00 +0800

    staffui 细节更新

--- src/main/java/presentation/staffui/hotelordermanage/Order_Manage_Controller.java
+++ src/main/java/presentation/staffui/hotelordermanage/Order_Manage_Controller.java
@@ -6,10 +6,14 @@
 
 package presentation.staffui.hotelordermanage;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Window;
 import presentation.utilui.WindowGrab;
 
 public class Order_Manage_Controller {
@@ -23,7 +27,16 @@ public class Order_Manage_Controller {
     @FXML
     private Button check;
 
-
+    private static URL ORDER_CHECK_FXML;
+    private static URL ORDER_CHECK_CSS;
+    static{
+    	try {
+    		ORDER_CHECK_FXML = new URL("file:src/main/resources/ui/staffui/fxml/order_check.fxml");
+    		ORDER_CHECK_CSS = new URL("file:src/main/resources/ui/staffui/css/order_check.css");
+    	} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+    }
 
     @FXML
     void cancel(ActionEvent event) {
@@ -32,7 +45,9 @@ public class Order_Manage_Controller {
 
     @FXML
     void check(ActionEvent event) {
-
+     	Window owner = WindowGrab.getWindow(event);
+    	WindowGrab.startWindow(owner, "订单信息", ORDER_CHECK_FXML, ORDER_CHECK_CSS);
+    
     }
 
 }
