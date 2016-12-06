ef12ad85067848349df8f5949c223e593d99404a
YYM <979579683@qq.com>
2016-12-06 15:40:42 +0800

    废弃包删除

--- src/main/java/presentation/loginui/LoginController.java
+++ src/main/java/presentation/loginui/LoginController.java
@@ -1,27 +1,18 @@
 package presentation.loginui;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import com.sun.javafx.robot.impl.FXRobotHelper;
-
 import config.URLConfig;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
-import javafx.stage.Stage;
 import javafx.stage.Window;
-import presentation.utilui.Dialog;
 import presentation.utilui.WarningLabel;
 import presentation.utilui.WindowGrab;
 
