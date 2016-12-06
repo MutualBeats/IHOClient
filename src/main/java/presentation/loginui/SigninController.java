46b50a5b0fc31a0ef8362b2b72fe784ff92eeac4
YYM <979579683@qq.com>
2016-12-06 13:49:07 +0800

    界面重构

--- src/main/java/presentation/loginui/SigninController.java
+++ src/main/java/presentation/loginui/SigninController.java
@@ -3,124 +3,27 @@ package presentation.loginui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import config.NumberConfig;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
-import presentation.utilui.WarningLabel;
+import presentation.utilcontroller.RegistCheckController;
 import presentation.utilui.WindowGrab;
 
-public class SigninController implements Initializable{
+public class SigninController extends RegistCheckController implements Initializable{
 	
-	@FXML Button confirm;
-	@FXML Button cancel;
-	@FXML TextField user_name;
-	@FXML TextField name;
-	@FXML TextField phone;
-	@FXML PasswordField ps;
-	@FXML PasswordField ps_again;
-	@FXML Label user_name_warning;
-	@FXML Label name_warning;
-	@FXML Label phone_warning;
-	@FXML Label ps_warning;
+	
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 	}
 	
 	@FXML
-	public void confirm(ActionEvent event) {
+	protected void confirm(ActionEvent event) {
 		boolean formatter_check = checkInputFormatter();
 		if(formatter_check) {
 			//TODO : Pass the check
 			
 		}
-	}
-	
-	private boolean checkInputFormatter() {
-		boolean user_name_check = checkUserName();
-		boolean name_check = checkName();
-		boolean phone_check = checkPhone();
-		boolean pass_check = checkPassword();
-		return user_name_check && name_check && phone_check && pass_check;
-	}
-	
-	//TODO : Connect the server and check the user_name is used or not
-	private boolean checkUserName() {
-		String user = user_name.getText();
-		int name_length = user.length();
-		if(name_length == 0) {
-			//Name shouldn't be empty
-			user_name_warning.setText("请输入用户名");
-			return false;
-		}
-		boolean user_name_check = NumberConfig.userNameFormatterCheck(user);
-		if(!user_name_check) {
-			//name formatter check
-			user_name_warning.setText("用户名应为8～12位数字或字母");
-		}
-		return user_name_check;
-	}
-	
-	/**
-	 * Check the name formatter
-	 */
-	private boolean checkName() {
-		String c_name = name.getText();
-		if(c_name.length() == 0) {
-			name_warning.setText("请输入您的姓名");
-			return false;
-		}
-		boolean name_check = NumberConfig.nameFormatterCheck(c_name);
-		if(!name_check) {
-			name_warning.setText("姓名应为1～12位，不包含空格等特殊字符");
-		}
-		return name_check;
-		
-	}
-	
-	/**
-	 * Check the phone formatter
-	 */
-	private boolean checkPhone() {
-		String ph = phone.getText();
-		if(ph.length() == 0) {
-			phone_warning.setText("请输入手机");
-			return false;
-		}
-		boolean ph_check = NumberConfig.phoneFormatterCheck(ph);
-		if(!ph_check) {
-			phone_warning.setText("手机号应为11位数字");
-		}
-		return ph_check;
-	}
-	
-	/**
-	 * Check the password formatter
-	 * Check the password input twice.
-	 */
-	private boolean checkPassword() {	
-		String pass = ps.getText();
-		String pass_again = ps_again.getText();
-		if(pass.length() == 0) {
-			ps_warning.setText("请输入密码");
-			return false;
-		}
-		boolean ps_check = NumberConfig.passFormatterCheck(pass);
-		if(!ps_check) {
-			ps_warning.setText("密码应为8～16位，且不含空格等特殊字符");
-			return false;
-		}
-		if(!pass.equals(pass_again)) {
-			ps_warning.setText("两次密码不相同");
-			return false;
-		}
-		return true;
 	}
 	
 	@FXML
@@ -131,30 +34,5 @@ public class SigninController implements Initializable{
     void b22e2e(ActionEvent event) {
 
     }
-	
-	@FXML
-	public void nameModify(MouseEvent event){
-		WarningLabel.checkWarningBefore(name_warning);
-	}
-	
-	@FXML
-	public void userNameModify(MouseEvent event){
-		WarningLabel.checkWarningBefore(user_name_warning);
-	}
-	
-	@FXML
-	public void phoneModify(MouseEvent event){
-		WarningLabel.checkWarningBefore(phone_warning);
-	}
-	
-	@FXML
-	public void passwordModify(MouseEvent event){
-		boolean warn_before = WarningLabel.checkWarningBefore(ps_warning);
-		if(warn_before) {
-			ps.setText("");
-			ps_again.setText("");
-		}
-	}
-	
-	
+		
 }
--- src/main/java/presentation/manageui/addpeople/AddPeopleController.java
+++ src/main/java/presentation/manageui/addpeople/AddPeopleController.java
@@ -1,46 +1,49 @@
 package presentation.manageui.addpeople;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.stage.Stage;
-import javafx.stage.Window;
-import presentation.utilui.WindowGrab;
+import javafx.scene.control.Label;
+import presentation.utilcontroller.RegistCheckController;
 
-public class AddPeopleController implements Initializable{
-	
-    @FXML
-    private Button cancel;
-    private static URL ADD_PEOPLE_CONFIRM_FXML;
-    private static URL ADD_PEOPLE_CONFIRM_CSS;
-    static{
-    	try {
-    		ADD_PEOPLE_CONFIRM_FXML = new URL("file:src/main/resources/ui/manageui/fxml/addpeopleconfirm.fxml");
-    		ADD_PEOPLE_CONFIRM_CSS = new URL("file:src/main/resources/ui/manageui/css/add_people_confirm.css");
-    	} catch (MalformedURLException e) {
-			e.printStackTrace();
-		}
-    }
-    @FXML
-    void cancel(ActionEvent event) {
-    	WindowGrab.closeWindow(event);
-    }
-    
-    @FXML
-    void confirm(ActionEvent event) {
-    	Window window = WindowGrab.getWindow(event);
-    	WindowGrab.startWindow(window, "确认添加", ADD_PEOPLE_CONFIRM_FXML,ADD_PEOPLE_CONFIRM_CSS);		   	
-    }
+public class AddPeopleController extends RegistCheckController implements Initializable {
+
+	@FXML
+	private Label addpeople_title;
+
+
+	// private static URL ADD_PEOPLE_CONFIRM_FXML;
+	// private static URL ADD_PEOPLE_CONFIRM_CSS;
+	// static{
+	// try {
+	// ADD_PEOPLE_CONFIRM_FXML = new
+	// URL("file:src/main/resources/ui/manageui/fxml/addpeopleconfirm.fxml");
+	// ADD_PEOPLE_CONFIRM_CSS = new
+	// URL("file:src/main/resources/ui/manageui/css/add_people_confirm.css");
+	// } catch (MalformedURLException e) {
+	// e.printStackTrace();
+	// }
+	// }
+
+	@FXML
+	protected void confirm(ActionEvent event) {
+		checkInputFormatter();
+		// Check the state
+		// Window window = WindowGrab.getWindow(event);
+		// WindowGrab.startWindow(window, "确认添加",
+		// ADD_PEOPLE_CONFIRM_FXML,ADD_PEOPLE_CONFIRM_CSS);
+	}
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
-		
+
+	}
+
+	void addPeople() {
+
 	}
 
 }
