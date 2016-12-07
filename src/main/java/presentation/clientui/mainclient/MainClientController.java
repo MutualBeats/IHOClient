2fc5c260fce6023d0b2434ff1f310e3c25593a2d
hhxianqu <735913885@qq.com>
2016-12-07 13:02:49 +0800

    client ui

--- src/main/java/presentation/clientui/mainclient/MainClientController.java
+++ src/main/java/presentation/clientui/mainclient/MainClientController.java
@@ -1,5 +1,74 @@
 package presentation.clientui.mainclient;
 
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import presentation.utilui.WindowGrab;
+
 public class MainClientController {
 
+    @FXML
+    private Pane pane_browseorder;
+
+    @FXML
+    private Label user_name;
+
+    @FXML
+    private Label createorder;
+
+    @FXML
+    private Label maintainmessage;
+
+    @FXML
+    private Button all_order;
+
+    @FXML
+    private Label title;
+
+    @FXML
+    private Label menu;
+
+    @FXML
+    private Label revokeorder;
+
+    @FXML
+    private Button execute_order;
+
+    @FXML
+    private Label evaluatehotel;
+
+    @FXML
+    private TextField input;
+
+    @FXML
+    private Button search;
+
+    @FXML
+    private Button revoked_order;
+
+    @FXML
+    private Button logout;
+
+    @FXML
+    private Label browseorder;
+
+    @FXML
+    private Button unexecute_order;
+
+    @FXML
+    private Label member;
+
+    @FXML
+    private Button unusual_order;
+
+    @FXML
+    void logout(ActionEvent event) {
+    	WindowGrab.closeWindow(event);
+    }
+
 }
+
