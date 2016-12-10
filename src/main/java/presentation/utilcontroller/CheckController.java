a3f78169b88000ba0cd45c1bb358009310d06ef6
hhxianqu <735913885@qq.com>
2016-12-10 12:37:11 +0800

    util 添加check界面

---
+++ src/main/java/presentation/utilcontroller/CheckController.java
@@ -0,0 +1,44 @@
+package presentation.utilcontroller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import presentation.utilui.WindowGrab;
+
+public class CheckController {
+
+    @FXML
+    private Label order_status;
+
+    @FXML
+    private Button return_;
+
+    @FXML
+    private Label order_id7;
+
+    @FXML
+    private Label expect_leave_time;
+
+    @FXML
+    private Label expect_check_in;
+
+    @FXML
+    private Label expect_population;
+
+    @FXML
+    private Label order_id;
+
+    @FXML
+    private Label hotel_name;
+
+    @FXML
+    private Label last_execute_time;
+
+    @FXML
+    void return_(ActionEvent event) {
+    	WindowGrab .closeWindow(event);
+    }
+
+}
+
